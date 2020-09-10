3f3eb41d0123500addd0eb928223865d48b6b9ca
Chiraphon Worasuk <apple10144@hotmail.com>
2020-09-11 01:06:12 +0700

    fix undg

--- src/templates/govtechDemoCert/certificate.tsx
+++ src/templates/govtechDemoCert/certificate.tsx
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+﻿import { format } from "date-fns";
 import { get } from "lodash";
 import React, { FunctionComponent } from "react";
 import "bootstrap/dist/css/bootstrap.css";
@@ -7,68 +7,82 @@ import { TemplateProps } from "@govtechsg/decentralized-renderer-react-component
 import { GovtechOpencertsTemplateCertificate } from "../samples";
 import { PrintWatermark } from "./common/PrintWatermark";
 //import mainLogo from "./common/opencertslogo.svg";
-import mainLogo from "./common/sut_logo.png";
+import sutLogo from "./common/sutlogo.png";
 import logo from "./common/SutLetter_logo.png";
 import certificateBg from "./common/certificate_background.png";
 
 export const CertificateTemplate: FunctionComponent<TemplateProps<GovtechOpencertsTemplateCertificate>> = ({
   document
-}) => (
-  <>
-    <PrintWatermark />
-    <div
-      className="p-2 container"
-      style={{
-        backgroundImage: `url('${certificateBg}')`,
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        border: "10px solid #324353"
-      }}
-    >
+}) => {
+  //const transcriptData = document.transcript || [];
+  var x: number = +(document.totalCredit);
+  if (x < 169){
+      return (<div className="my-5 m-lg-5 text-center" style={{ paddingTop: "5%"}}> 
+        <font size="4"> Undergraduate </font>
+        </div>);
+  }
+  else {
+  return (
       <div
         className="p-2"
         style={{
           border: "0px solid #324353"
         }}
       >
-        <div className="my-5 m-lg-5 text-center">
-          <img src={mainLogo} alt="SUT Logo" style={{ width: "100%", height: "100%", maxWidth: "1000px" }} />
-        </div>
-        <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-body" style={{ textAlign: "center" }}>
-          <i>This is to certify that</i>
-        </div>
-        <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-title">
-          <b>{document.recipient.name}</b>
-        </div>
-        <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-body" style={{ textAlign: "center" }}>
-            <i>has been conferred the degree of Bachelor of {document.recipient.bname}</i>
-        </div>
-        {/* <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-title" style={{ textAlign: "center" }}>
-            {document.recipient.bname}
-        </div> */}
-        <div className="mb-4 mb-lg-5 d-flex justify-content-center cert-body" style={{ textAlign: "center" }}>
-          <i>with the approval of the Suranaree University of Technology Council.</i>
-        </div>
-        <div className="row">
-          <div className="col" />
-          <div className="col">
-	  <div align="center">
-            <img
-              className="w-100"
-              style={{ width: "100", height: "auto", maxWidth: "100px" }}
-              src={logo}
-                alt="SutLetter Logo"
-            />
+
+        <div style={{paddingTop: "2%"}}>
+          <div className="text-center" ><img src={sutLogo} alt="SUT Logo" 
+          style={{ width: "10%", height: "10%"}} />
+           <div className="tranhead text-left" style={{ paddingLeft: "60%", lineHeight: "40px"}}>
+           <font size="4">Suranaree University of Technology<br/>
+           111 University Avenue<br />
+           Muang District, Nakhon Ratchasima 30000</font>
+          </div></div>
+        
           </div>
-	</div>
-          <div className="col" />
+
+        <div className="my-5 m-lg-5 text-center" style={{ paddingTop: "7%"}}> 
+        <font size="4"> This is to certify that</font>
+        </div>
+        <div className="my-5 m-lg-5 cert-body" style={{ textAlign: "center"}}>
+          <font size="5"><b>
+          <div style={{display: "inline", textTransform: "capitalize"}}>
+          {document.gender}</div> <div style={{display: "inline", textTransform: "uppercase"}}>
+          {document.recipient.name}</div></b></font><br/>
+          <font size="4">(Student Identification Number: {document.studentID})</font>
+        </div>
+        
+        <div className="my-5 text-left" style={{paddingLeft: "5%", lineHeight: "40px"}}>
+          <font size="4">has been conferred the degree of Bachelor of {document.recipient.bname} for the Academic Year {document.lastyear} since<br />
+           {document.graduationDate} with the approval of the Suranaree University of Technology Councill.
+            </font>
+        </div>
+        <div className="text-left" style={{paddingLeft: "15%"}}>
+          <font size="4">Given on {document.issuedOn}
+            </font>
+        </div>
+      <div className="text-center" style={{paddingLeft: "25%", paddingTop: "10%", lineHeight: "40px"}}>
+          <font size="4">
+          <b>{get(document, "additionalData.certSignatories[0].name")}</b><br/>
+          (Miss {get(document, "additionalData.certSignatories[0].name")})<br/>
+          Acting Chief, Evaluation and Graduate Informaion Section<br/>
+              Assistant Regiser<br />
+            </font>
+        </div>
+            <div className="text-left" style={{paddingLeft: "8%", paddingTop: "20%"}}>
+         <font size="3">National Identification Number {document.identNum}
+            </font>
+        </div>
+        <div className="my-5 m-lg-2 text-center" style={{ textAlign: "center", paddingTop: "12%"}}> 
+         <font size="4"><b>(NOT VALID WITHOUT UNIVERSITY SEAL)</b></font>
         </div>
 
+     {/*
         <div
           className="row"
           style={{
-            paddingLeft: "8%",
-            paddingTop: "5%"
+            paddingRight: "8%",
+            paddingTop: "15%"
           }}
         >
           <div className="col text-center transcript">
@@ -102,8 +116,8 @@ export const CertificateTemplate: FunctionComponent<TemplateProps<GovtechOpencer
           >
             Dated {format(document.issuedOn, "DD/MM/YYYY")}
           </div>
-        </div>
+        </div>*/}
       </div>
-    </div>
-  </>
-);
+  );
+  }
+};
